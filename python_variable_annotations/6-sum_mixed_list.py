3f5a88c22743ab61f4dd2adf9857d56b341e36f1
SebSa12000 <sebastien.salgues@gmail.com>
2025-07-28 16:10:22 +0200

    permut elem

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float,int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     ''' Sum mixed floats / int '''
     sum = 0
     for item in mxd_lst:
