4df352b5ae31c874ef554e50498b3b1beca7a2d4
SebSa12000 <sebastien.salgues@gmail.com>
2025-07-28 16:23:52 +0200

    task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' k v function '''
+
+
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: List[Union[int, float]]) -> Tuple[str, float]:
+    ''' to_kv '''
+    return (k, v * v)
