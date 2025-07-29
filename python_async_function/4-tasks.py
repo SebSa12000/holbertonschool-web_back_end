c08ed0dbd4780fdb55faffb9d0524c984fbaf155
SebSa12000 <sebastien.salgues@gmail.com>
2025-07-29 10:01:29 +0200

    task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,7 +1,14 @@
-import time
+#!/usr/bin/env python3
+"""executing multiple tasks"""
+
+
+from typing import List
 import asyncio
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_n(n: int, max_delay: int) -> float:
-    return asyncio.run(wait_n(n, max_delay))
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    '''wait random n fois '''
+    delays = [wait_random(max_delay) for _ in range(n)]
+    delay: List[float] = await asyncio.gather(*delays)
+    return sorted(delay)
