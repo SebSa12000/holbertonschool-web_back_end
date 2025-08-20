59ca574a732adb2bafc2d1990a29e99d6f61d2ae
SebSa12000 <sebastien.salgues@gmail.com>
2025-08-20 11:00:59 +0200

    task 11

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -2,5 +2,5 @@ export default function createEmployeesObject(departmentName, employees) {
     var list = [];
     for (let emp of employees)
         list.push(emp);
-    return { departmentName : list };
+    return { [departmentName] : list };
 }
